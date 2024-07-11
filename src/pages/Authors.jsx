b6de8c885a8ad72f26f9a649da422e507381066e
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-12 00:58:25 +0300

    Header, Footer and Chatbot component added

--- src/pages/Authors.jsx
+++ src/pages/Authors.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Chatbot from '../components/Chatbot'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Authors = () => {
   return (
     <div>
+      <Header/>
+      <Chatbot />
       Authors
+      <Footer />
     </div>
   )
 }
