2d782db36cced80875717598da8e259666d2a40f
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-11 08:33:46 +0300

    responsive blogs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Blogs from './components/Blogs'
 import './App.css'
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
 
   return (
     <>
-     
+     <Blogs/>
     </>
   )
 }
