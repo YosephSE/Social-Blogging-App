b0f69b957cd83ec19823ae30bab0e7001f3f484e
eyoelm5 <eyoelmoges20@gmail.com>
2024-07-11 12:17:15 +0300

    Adding Authors and Create post page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Authors from './components/Authors'
+import CreateBlog from './components/CreateBlog'
 
 function App() {
   
 
   return (
     <>
-     
+      <CreateBlog />
     </>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,33 @@
+export default{
+    "people":[
+        {
+            fullName: "Max Carter",
+            url: "https://imagetolink.com/ib/MQEShazNG6.jpg",
+            post: 3
+        },
+
+        {
+            fullName: "Sophie Miller",
+            url: "https://imagetolink.com/ib/HjjFcyXHmQ.png",
+            post: 1
+        },
+
+        {
+            fullName: "Lily Thompson",
+            url: "https://imagetolink.com/ib/C4Jt0NDJeQ.png",
+            post: 2
+        },
+
+        {
+            fullName: "Emily Johnson",
+            url: "https://imagetolink.com/ib/PgUW4VNJDd.png",
+            post: 2
+        },
+
+        {
+            fullName: "John Doe",
+            url: "https://imagetolink.com/ib/IdNTuyJJbI.png",
+            post: 1
+        }
+    ] 
+}
