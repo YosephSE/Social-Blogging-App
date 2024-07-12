476a9622b56120224abdf5a278429b1baa237c0b
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-12 22:20:05 +0300

    API key updated

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Chatbot = () => {
   return (
     <div>
-      <Link target="_blank" to="https://chatgpt.com">
+      <Link target="_blank" to="https://zeresenayyaregalchatbot.streamlit.app/">
         <img
           src={chatbot}
           alt="Fixed Image"
