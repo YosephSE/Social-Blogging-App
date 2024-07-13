faeed1a46091cec98079948f2e4ab482cf3cd81f
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-13 20:54:00 +0300

    chatbot icon added

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,25 +1,19 @@
 import React, { useState } from 'react';
+import botIcon from "../assets/chatbot.png"
 
 const Chatbot = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="fixed bottom-4 right-4">
-      <div className={`transition-transform transform ${isOpen ? 'translate-y-0' : 'translate-y-full'} fixed bottom-4 right-4 bg-white shadow-lg w-1/5 h-1/2`}>
+      <div className={`transition-transform transform ${isOpen ? 'translate-y-0' : 'translate-y-full'} fixed bottom-4 right-4 bg-white shadow-lg w-2/5 h-2/3`}>
         <iframe
           src="https://zeresenayyaregalchatbot.streamlit.app/"
           className="w-full h-full"
           title="Chatbot"
         ></iframe>
       </div>
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="bg-blue-500 text-white p-2 rounded-full shadow-lg"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m13 3a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-      </button>
+      <img src={botIcon} alt="BotIcon" />
     </div>
   );
 };
