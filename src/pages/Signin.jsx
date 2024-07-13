7a388abd0c0660623f5f66bb2db88fbf43a19b0d
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-13 17:04:44 +0300

    Link imported

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
+import { Link } from "react-router-dom";
 
 function Signin() {
   const [email, setEmail] = useState('');
