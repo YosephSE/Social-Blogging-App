e134213056862a5702415ae4250365d748ff542f
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-14 00:01:35 +0300

    hash route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from "react"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Blogs from "./pages/Blogs";
 import MyPosts from "./pages/MyPosts";
 import Signin from "./pages/Signin";
