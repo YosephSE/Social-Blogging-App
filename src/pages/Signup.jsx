f2e10f58f386fa62f89efd1493d033578a61704c
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-03 00:43:07 +0300

    deplicate account filtering added

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from '../components/Header';
-import profilePicture from "../assets/profilepicture.png"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import profilePicture from "../assets/profilepicture.png";
 import { Link } from "react-router-dom";
 
-function Signup({data , dataChange}) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+function Signup({ data, dataChange }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const navigate = useNavigate();
 
@@ -20,11 +20,8 @@ function Signup({data , dataChange}) {
       return;
     }
 
-    const userExists = data.filter(user => user.email === email)
-    if(userExists){
-      setError('Account exists please login!');
-    }
-    else {
+    const userExists = data.filter((user) => user.email === email);
+    if (userExists.length === 0) {
       const userData = {
         name,
         email,
@@ -34,88 +31,95 @@ function Signup({data , dataChange}) {
 
       let users = data;
 
-      
       users.push(userData);
 
-      dataChange(prevData => ({
+      dataChange((prevData) => ({
         ...prevData,
-        users: users
-      }))
+        users: users,
+      }));
 
-      navigate('/signin');
+      navigate("/signin");
+    } else {
+      setError("Account exists please login!");
     }
   };
 
   return (
     <>
-    <Header />
-    <div className='min-h-[calc(100vh-80px)] flex flex-col'>
-    <div className="flex items-center justify-center bg-gray-200 w-full flex-grow">
-      <div className="w-full max-w-xs">
-        <form className="rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-          <h2 className="text-center text-2xl mb-6">Sign up</h2>
-          <div className="mb-4">
-            <input
-              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="name"
-              type="text"
-              placeholder="Full Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="confirmPassword"
-              type="password"
-              placeholder="Confirm Password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-          </div>
-          {error && (
-            <p className="text-red-500 text-xs italic mb-4">{error}</p>
-          )}
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
+      <Header />
+      <div className="min-h-[calc(100vh-80px)] flex flex-col">
+        <div className="flex items-center justify-center bg-gray-200 w-full flex-grow">
+          <div className="w-full max-w-xs">
+            <form
+              className="rounded px-8 pt-6 pb-8 mb-4"
+              onSubmit={handleSubmit}
             >
-              Register
-            </button>
+              <h2 className="text-center text-2xl mb-6">Sign up</h2>
+              <div className="mb-4">
+                <input
+                  className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="name"
+                  type="text"
+                  placeholder="Full Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="confirmPassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+              </div>
+              {error && (
+                <p className="text-red-500 text-xs italic mb-4">{error}</p>
+              )}
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Register
+                </button>
+              </div>
+              <p className="text-center text-gray-500 text-xs mt-4">
+                Already have an account?{" "}
+                <span className="text-blue-500">
+                  <Link to="/signin">Sign In</Link>
+                </span>
+              </p>
+            </form>
           </div>
-          <p className="text-center text-gray-500 text-xs mt-4">
-            Already have an account? <span className='text-blue-500'><Link to="/signin" >Sign In</Link></span>
-          </p>
-        </form>
+        </div>
       </div>
-    </div>
-    </div>
     </>
   );
 }
