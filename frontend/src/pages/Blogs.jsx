28580d0c0ff9c0f113c0fe02a5e7a28f9815eabe
eyoelm5 <eyoelmoges20@gmail.com>
2024-08-08 23:47:46 +0300

    Change the UI of blogs

--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -10,23 +10,36 @@ const Blogs = ({data}) => {
     <div className="flex flex-col min-h-screen">
       <Header page='Blogs' data = {data.posts} dataChange = {setPosts} />
       <Chatbot />
-      <div className="blogs flex-grow grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-4 bg-gray-300 px-2 md:px-8 md:py-3 py-1">
-        {posts.map((post) => {
-          const user = data.users.filter((user) => user.name === post.name);
-          return (
-            <Blog
-              name={post.name}
-              date={post.date}
-              key={post.id}
-              title={post.title}
-              category={post.category}
-              body={post.body}
-              comments={post.comments}
-              profilePicture={user[0]?.profilePicture}
-              img={post.img}
-            />
-          );
-        })}
+      <div className="md:grid-cols-[220px_1fr] pt-10 md:grid md:items-start py-4 flex flex-col items-center bg-gray-300">
+        <ul className="mx-5 my-12 hidden flex-col gap-2 font-semibold md:flex">
+          <li className="text-2xl font-bold">All Categories</li>
+          <li>All Blogs</li>
+          <li>Technology</li>
+          <li>Art</li>
+          <li>Education</li>
+          <li>Nutrition</li>
+          <li>Fashion</li>
+          <li>Music</li>
+          <li>Uncatagorized</li>
+        </ul>
+        <div className="blogs flex-grow grid grid-cols-1 gap-4 bg-gray-300 px-2 md:px-8 md:py-3 py-1">
+          {posts.map((post) => {
+            const user = data.users.filter((user) => user.name === post.name);
+            return (
+              <Blog
+                name={post.name}
+                date={post.date}
+                key={post.id}
+                title={post.title}
+                category={post.category}
+                body={post.body}
+                comments={post.comments}
+                profilePicture={user[0]?.profilePicture}
+                img={post.img}
+              />
+            );
+          })}
+        </div>
       </div>
       <Footer />
     </div>
