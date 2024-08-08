7be3dfa182ce27e761887e4325360a52146df5cf
eyoelm5 <eyoelmoges20@gmail.com>
2024-08-09 00:04:10 +0300

    Add profile picture to backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,7 +31,7 @@ const authUser = asyncHandler(async (req, res) => {
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, profilePicture } = req.body;
   const userExists = await User.findOne({ email });
 
   if (userExists) {
@@ -39,11 +39,11 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User Already Exists");
   }
 
-  const user = await User.create({ name, email, password });
+  const user = await User.create({ name, email, password, profilePicture });
 
   if (user) {
     generateToken(res, user._id);
-    res.status(201).json({ _id: user._id, name: user.name, email: user.email });
+    res.status(201).json({ _id: user._id, name: user.name, email: user.email, profilePicture: user.profilePicture });
   } else {
     throw new Error("Invalid user Data");
   }
@@ -62,6 +62,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
     _id: req.user._id,
     name: req.user.name,
     email: req.user.email,
+    profilePicture: req.user.profilePicture
   };
   res.status(200).json(user);
 });
@@ -72,6 +73,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
+    user.profilePicture = req.body.profilePicture || user.profilePicture;
 
     if (req.body.password) {
       user.password = req.body.password;
@@ -83,6 +85,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
         _id: updatedUser._id,
         name: updatedUser.name,
         email: updatedUser.email,
+        profilePicture: updatedUser.profilePicture
       });
   } else {
     res.status(404);
