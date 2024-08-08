40c79c9fef718b20ecc80e7c5450831a758cd1b7
eyoelm5 <eyoelmoges20@gmail.com>
2024-08-08 23:45:47 +0300

    Link front end with backend by using proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Social-Blogging-App"
+  base: "Social-Blogging-App",
+  proxy:{
+    '/api': {
+      target: 'http://localhost:5000',
+      changeOrigin: true
+    }
+  }
 })
