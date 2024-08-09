9c62add2f56f4c16acf1c9916d41a44441230f38
eyoelm5 <eyoelmoges20@gmail.com>
2024-08-09 16:57:23 +0300

    Fix 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Social-Blogging-App",
+  base: "Social-Blogging-app-front-end",
 })
