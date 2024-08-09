1b4eb160193b39244a41c3e577b523e267d60328
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-09 10:20:36 +0300

    all authors and myposts added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,6 +6,7 @@ import {
   logoutUser,
   getUserProfile,
   updateUserProfile,
+  allAuthors,
 } from "../controllers/userController.js";
 import { protect } from "../middlware/authmiddleware.js";
 
@@ -17,5 +18,6 @@ Router.post("/auth", authUser);
 Router.post("/logout", logoutUser);
 Router.get("/profile", protect, getUserProfile);
 Router.put("/profile", protect, updateUserProfile);
+Router.get("/authors", allAuthors);
 
 export default Router;
